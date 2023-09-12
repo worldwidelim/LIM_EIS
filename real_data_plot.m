de7ba3fa4338b5064257c5c33d58a4576c0bd75b
worldwidelim <135565563+worldwidelim@users.noreply.github.com>
2023-09-12 14:26:14 +0900

    Add files via upload

---
+++ real_data_plot.m
@@ -0,0 +1,34 @@
+clear; clc; close all;
+
+% Set the filename
+filename = 'C:\Users\hyoju\Desktop\eis\eis_data.txt';
+
+% Open the file for reading
+fileID = fopen(filename, 'r');
+
+% Skip the first row (header)
+fgetl(fileID);
+
+% Read the data
+data = fscanf(fileID, '%f\t%f\t%f', [3, Inf]);
+
+% Close the file
+fclose(fileID);
+
+% Store data in variables
+freq = data(1, :);
+Re_Z = data(2, :);
+Im_Z = -data(3, :);
+
+% Plot
+figure;
+plot(Re_Z, -Im_Z, 'b', 'LineWidth', 1.5);
+xlabel('Re(Z) (Ohm)');
+ylabel('Im(Z) (Ohm)');
+title('Impedance');
+legend('real data');
+
+
+
+
+
