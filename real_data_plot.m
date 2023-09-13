39f88e5d7f2f3a0d3e455063282a387457ec3500
worldwidelim <135565563+worldwidelim@users.noreply.github.com>
2023-09-13 15:56:23 +0900

    Update real_data_plot.m

--- real_data_plot.m
+++ real_data_plot.m
@@ -29,6 +29,28 @@ title('Impedance');
 legend('real data');
 
 
+%% Use 'readtable' to create a table from the data file
 
+clear; clc; close all;
 
+% Set the filename
+filename = 'C:\Users\hyoju\Desktop\eis\eis_data.txt';
 
+% Read the data using readtable
+data = readtable(filename, 'Delimiter', '\t', 'HeaderLines', 1, 'ReadVariableNames', false);
+
+% Rename the variables
+data.Properties.VariableNames = {'freq', 'Re_Z', 'Im_Z'};
+
+% Extract data from the table
+freq = data.freq;
+Re_Z = data.Re_Z;
+Im_Z = -data.Im_Z;
+
+% Plot
+figure;
+plot(Re_Z, -Im_Z, 'b', 'LineWidth', 1.5);
+xlabel('Re(Z) (Ohm)');
+ylabel('-Im(Z) (Ohm)');
+title('Impedance');
+legend('real data');
