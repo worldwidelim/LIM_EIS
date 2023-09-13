274b8bd7906cfad1266bf542be3bf36dcf61f494
worldwidelim <135565563+worldwidelim@users.noreply.github.com>
2023-09-13 16:41:41 +0900

    Update yes_weight.m

--- yes_weight.m
+++ yes_weight.m
@@ -22,7 +22,7 @@ z_syn = z_mod + z_re_noise + 1i*z_im_noise;
 w_switch = 0; % choose between minimizing (0) absolute error; (1) relative error
 % if minimizing the relative error
 if w_switch == 1
-    weight = (z_syn.^2 + abs(z_syn).^2).^0.5; % 수정
+    weight = (real(z_syn).^2 + imag(z_syn).^2).^0.5;
     weight_matrix = [weight weight];
 % if minimizing the absolute error
 elseif w_switch == 0
