c478330b1b915513130047d0109bd5826be45b0c
worldwidelim <135565563+worldwidelim@users.noreply.github.com>
2023-09-22 03:30:09 +0900

    Update real_data_fitting_coth.m

--- real_data_fitting_coth.m
+++ real_data_fitting_coth.m
@@ -3,22 +3,16 @@ clear; clc; close all;
 % Set the filename
 filename = 'C:\Users\hyoju\Desktop\eis\eis_data.txt';
 
-% Open the file for reading
-fileID = fopen(filename, 'r');
+% Read the data using readtable
+data = readtable(filename, 'Delimiter', '\t', 'HeaderLines', 1, 'ReadVariableNames', false);
 
-% Skip the first row (header)
-fgetl(fileID);
+% Rename the variables
+data.Properties.VariableNames = {'freq', 'Re_Z', 'Im_Z'};
 
-% Read the data
-data = fscanf(fileID, '%f\t%f\t%f', [3, Inf]);
-
-% Close the file
-fclose(fileID);
-
-% Save data in variables
-freq = data(1, :);
-Re_Z = data(2, :);
-Im_Z = -data(3, :);
+% Extract data from the table
+freq = data.freq;
+Re_Z = data.Re_Z;
+Im_Z = -data.Im_Z;
 
 % Use the frequency from the data
 w = 2 * pi * freq;
@@ -28,26 +22,29 @@ R1 = 0.03;
 R2 = 0.017;
 C = 0.1;
 A = 0.03;
-params = [R1, R2, C, A];
+t = 100; % t는 시상수 τ를 나타냄
+params = [R1, R2, C, A, t];
 
 % model calculation
 z_mod = z_model(w, params);
 
 % fitting
 options = optimoptions('fmincon', 'Display', 'iter');
-params_fit = fmincon(@(params) rmse(z_model(w, params), Re_Z - 1i * Im_Z), params, [], [], [], [], [0, 0, 0, 0], [Inf, Inf, Inf, Inf], [], options);
+params_fit = fmincon(@(params) rmse(z_model(w, params), Re_Z - 1i * Im_Z), params, [], [], [], [], [0, 0, 0, 0, 0], [Inf, Inf, Inf, Inf, Inf], [], options);
 
 % Extract the fitted parameters
 R1_fit = params_fit(1);
 R2_fit = params_fit(2);
 C_fit = params_fit(3);
 A_fit = params_fit(4);
+t_fit = params_fit(5);
 
 % Display fitted parameters
 disp(['Fitted R1: ', num2str(R1_fit)]);
 disp(['Fitted R2: ', num2str(R2_fit)]);
 disp(['Fitted C: ', num2str(C_fit)]);
 disp(['Fitted A: ', num2str(A_fit)]);
+disp(['Fitted t: ', num2str(t_fit)]);
 
 % Plot real data and fitted curve
 figure;
@@ -69,7 +66,8 @@ function [Z] = z_model(w, params)
     R2 = params(2);
     C = params(3);
     A = params(4);
-    t = R2 * C; % t는 시상수 τ를 나타냄
+    t = params(5);
+    t = R2 * C;
     Z_W = A * coth(sqrt(1i*w*t)) ./ sqrt(1i*w*t);
     Z_C = 1./(1i*w*C);
     Z = R1 + (R2 + Z_W) .* Z_C ./ ((R2 + Z_W) + Z_C);
